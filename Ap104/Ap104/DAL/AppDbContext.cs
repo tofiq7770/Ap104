f4eed221402482d52127b56c155ec6a6bc44304d
tofiq7770 <tofigtn@code.edu.az>
2023-12-14 08:39:27 +0400

    First Api Created

---
+++ Ap104/Ap104/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ap104.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ap104.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
